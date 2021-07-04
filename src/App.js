cd82f79dfeba5f058dd9404450aaae466c23c97f
gavatron77 <gavatron77@outlook.com>
2021-07-04 01:45:17 -0700

    'initapp'

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, {ReactDOM} from 'react';
+import Game from './components/Game';
+
+function App() {
+    return (
+        <div>
+            <Game></Game>
+        </div>
+    );
+}
+
+export default App;
